2b8724b53b3bfe771c0a7dfc3ffa3f5254bdf688
Tuan Ha <hatuan@email.cz>
2025-08-24 22:22:56 +0200

    Add custom Date in object creation argument

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ console.log("Hello World!");
 const task = new Task();
 console.log(task);
 
-const task1 = new Task("Create Project", "Do this to pass semester", new Date(), 3, false, "Try your best");
+const task1 = new Task("Create Project", "Do this to pass semester", new Date(2025, 11, 24), 3, false, "Try your best");
 console.log(task1);
