dc229bf18bf7dcf544cb8f8e80f28452d748c0c6
Tuan Ha <hatuan@email.cz>
2025-08-24 22:18:41 +0200

    Create Task class with properties and accessors
    Task class also contains getters and setters for
    every defined property. Task objects are created
    in index.js file to test if they are created
    without issues.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import { Task } from "./todo.js";
+
 console.log("Hello World!");
+
+const task = new Task();
+console.log(task);
+
+const task1 = new Task("Create Project", "Do this to pass semester", new Date(), 3, false, "Try your best");
+console.log(task1);
---
+++ src/todo.js
@@ -0,0 +1,54 @@
+/**
+ * This class is a template for single tasks that
+ * the user can create.
+ * Tasks will be contained in a "Project"
+ */
+class Task {
+  #title;
+  #description;
+  #dueDate;
+  #priority;
+  #checked
+  #notes;
+
+  /**
+   * 
+   * @param {string} title the title of the tasks at hand (todo item)
+   * @param {string} description a short description of the todo item
+   * @param {Date} dueDate a date object indicating its deadline
+   * @param {number} priority a number from 1 - 3 indicating the priority (3 highest)
+   * @param {boolean} checked a togle between checked and unchecked task
+   * @param {string} notes a longer description of the given task
+   */
+  constructor(
+    title = "My Task"
+    , description = "Let's do this!"
+    , dueDate = new Date()
+    , priority = 1
+    , checked = false
+    , notes = "I am so lazy:(") {
+    this.#title = title;
+    this.#description = description;
+    this.#dueDate = dueDate;
+    this.#priority = priority;
+    this.#checked = checked;
+    this.#notes = notes;
+  }
+
+  get title() { return this.#title; }
+  get description() { return this.#description; }
+  get dueDate() { return this.#dueDate; }
+  get priority() { return this.#priority; }
+  get checked() { return this.#checked; }
+  get notes() { return this.#notes; }
+
+  set title(title) {this.#title = title}
+  set description(description) {this.#description = description}
+  set dueDate(dueDate) {this.#dueDate = dueDate}
+  set priority(priority) {this.#priority = priority}
+  set checked(checked) {this.#checked = checked}
+  set notes(notes) {this.#notes = notes}
+
+}
+
+export { Task };
