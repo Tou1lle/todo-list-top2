6eb7cb12d2cecf924daec565c5cb325796e5d265
Tuan Ha <hatuan@email.cz>
2025-09-23 16:18:07 +0200

    Remove redundant import

--- src/script_modules/ui/project_view.js
+++ src/script_modules/ui/project_view.js
@@ -2,7 +2,6 @@
 // -> We need to import this module to access the object that contains out Projects
 // -> We can't run ProjectMenuController -> we would create a new ProjectManager
 // -> Solution: Inject the ProjectMenuController/ProjectManager
-import { ProjectMenuController } from "./project_menu";
 
 function ProjectViewController(projectManagerArg) {
   const projectManager = projectManagerArg;
