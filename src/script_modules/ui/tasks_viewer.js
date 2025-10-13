f1f776255688bd24f6676a22bae2f41cd3764b0d
Tuan Ha <hatuan@email.cz>
2025-10-13 14:35:45 +0200

    Reset form when dialog closed

--- src/script_modules/ui/tasks_viewer.js
+++ src/script_modules/ui/tasks_viewer.js
@@ -1,22 +1,42 @@
 import { ProjectManager } from "../logic/project-manager";
 
 /**
- * 
+ * TaskViewer displays and adds Tasks to selected Project
+ * Other functionalities: sorting and filtering tasks based on user preferences
  * @param {ProjectManager} projectManager 
  */
 function TaskViewer(projectManagerArg) {
   const projectManager = projectManagerArg;
   const addTaskBtn = document.querySelector(".add-new-task");
   const dialog = document.querySelector("dialog");
+  const form = document.querySelector(".form-task");
+  const btnCancel = document.querySelector(".form-cancel-btn");
+  const btnSubmit = document.querySelector(".form-submit-btn");
 
   //return array of tasks
   function getSelectedTasks() {
     return projectManager.getSelected().tasks;
   }
 
+  function resetForm() {
+    form.reset();
+  }
+
+  function openDialog() {
+    dialog.showModal();
+  }
+
+  function closeDialog() {
+    dialog.close();
+  }
+
+  dialog.addEventListener("close", (e) => {
+    resetForm();
+  })
+
   addTaskBtn.addEventListener("click", (e) => {
     console.log("Add Tasks button works");
-    dialog.showModal();
+    openDialog();
   });
 }
 
